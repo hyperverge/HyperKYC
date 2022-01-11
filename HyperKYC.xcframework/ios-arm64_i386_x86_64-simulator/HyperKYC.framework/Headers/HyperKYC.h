ca57711aa8137410cc956db629e7248d96ca72ad
shriramaupadhyaa <shrirama.upadhya@hyperverge.co>
2022-01-11 16:00:35 +0530

    push test version 0.0.1

---
+++ HyperKYC.xcframework/ios-arm64_i386_x86_64-simulator/HyperKYC.framework/Headers/HyperKYC.h
@@ -0,0 +1,18 @@
+//
+//  HyperKYC.h
+//  HyperKYC
+//
+//  Created by Shrirama Upadhya A on 21/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HyperKYC.
+FOUNDATION_EXPORT double HyperKYCVersionNumber;
+
+//! Project version string for HyperKYC.
+FOUNDATION_EXPORT const unsigned char HyperKYCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HyperKYC/PublicHeader.h>
+
+
