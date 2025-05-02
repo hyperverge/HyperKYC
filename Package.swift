4d4922f56c64040261c3823a71a1bffe0b635a19
Nivedita <mnive93@gmail.com>
2025-05-02 11:55:23 +0530

    updated 0.44.0-spm

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
              .package(
                  name: "HyperSnapSDK",
                  url: "https://github.com/hyperverge/capture-ios-sdk.git",
-                 from: "4.35.0-spm"
+                 branch: "release/4.35.0-spm"
              )
     ],
     targets: [
